6f28d42d5430b46096716e8ddc6b09e7909a7c79
Shaw <michele_aliffi@yahoo.it>
2022-01-04 16:24:48 +0100

    Fine progetto aggiornato

--- src/main/java/org/generation/italy/controller/IndexController.java
+++ src/main/java/org/generation/italy/controller/IndexController.java
@@ -7,27 +7,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// GRUPPO: Sergio, Fabio, Marco, Mattia M, Jerome, Michele
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("author", "RottenTomatoes");
+		model.addAttribute("author", "Sergio");
 		return "index";
 	}
-	
+
 	@GetMapping("/movies")
 	public String movies(Model model) {
 		model.addAttribute("title", "Best Movies of 2021");
-		model.addAttribute("list", Arrays.asList("My Salinger Year", "Palmer", "Blu Bayou", "Cruella", "Percy VS Goliath"));
+		model.addAttribute("list",
+				Arrays.asList("My Salinger Year", "Palmer", "Blu Bayou", "Cruella", "Percy VS Goliath"));
 		return "list";
 	}
-	
+
 	@GetMapping("/songs")
 	public String songs(Model model) {
 		model.addAttribute("title", "Best Songs of 2021");
-		model.addAttribute("list", Arrays.asList("2010 — Earl Sweatshirt", "Keep An Eye on Dan — ABBA","Like I Used To — Sharon Van Etten & Angel Olsen"));
+		model.addAttribute("list", Arrays.asList("2010 — Earl Sweatshirt", "Keep An Eye on Dan — ABBA",
+				"Like I Used To — Sharon Van Etten & Angel Olsen"));
 		return "list";
 	}
 }
